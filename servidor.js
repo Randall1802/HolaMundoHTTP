a4e3265b365dc025550eb898cb702fd0d2c6e17c
rhs-dnu <rhs@dnu.com.mx>
2024-03-11 12:29:20 -0600

    Commit funcional del server

--- servidor.js
+++ servidor.js
@@ -35,7 +35,7 @@ http.createServer(function (request, response) {
     fs.readFile(filePath, function (error, content){
         if(error){
             if(error.code='ENOENT'){
-                fs.readFile('./404.html', function(error, content){
+                fs.readFile('./pag.html', function(error, content){
                     response.writeHead(200, {'Content-Type':contentType});
                     response.end(content, 'utf-8');
                 });
@@ -52,4 +52,4 @@ http.createServer(function (request, response) {
         }
     });
 }).listen(3000);
-console.log('Server running at http://127.0.0.1:3000');
+console.log('Server running at http://192.168.243.7:3000');
